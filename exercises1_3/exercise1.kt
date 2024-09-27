484feb5aa6bbb70569f909f92ef8c117f2b26e0d
W1lden <99272461+W1lden@users.noreply.github.com>
2024-09-27 16:13:39 +0500

    Update exercise1.kt

--- exercises1_3/exercise1.kt
+++ exercises1_3/exercise1.kt
@@ -21,17 +21,3 @@ fun main() {
         j++
     }
 }
-
-// fun main() {
-//     // Цикл for
-//     for (i in 1..10) {
-//         println(i)
-//     }
-
-//     // Цикл while
-//     var j = 1
-//     while (j <= 10) {
-//         println(j)
-//         j++
-//     }
-// }
